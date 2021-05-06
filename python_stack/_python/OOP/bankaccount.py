da208246fb7cc5aa86ba928f0fc1bee41c260bbb
Dara <dararateb93@gmail.com>
2021-05-06 15:32:04 +0300

    python assignments

--- python_stack/_python/OOP/bankaccount.py
+++ python_stack/_python/OOP/bankaccount.py
@@ -1,27 +1,25 @@
 class BankAccount:
     def __init__(self, int_rate, balance): 
-        self.int_rate=0
+        self.int_rate=int_rate
         self.balance=balance
     def deposit(self, amount):
         self.balance=self.balance+amount
+        return self
     def withdraw(self, amount):
         self.balance=self.balance-amount
+        return self
     def display_account_info(self):
         print(f"Interest Rate: {self.int_rate} Balance: {self.balance}")
+        return self
     def yield_interest(self,int_rate):
         self.balance=self.balance+(self.balance*self.int_rate)
+        return self
 
 Dara=BankAccount(0.05,5000)
 Amira=BankAccount(0.03,3000)
-Dara.display_account_info()
+#Dara.display_account_info()
 
-Dara.deposit(3000)
-Dara.deposit(1000)
-Dara.deposit(5000)
-Dara.withdraw(4000)
-Dara.yield_interest(0.5)
-Dara.display_account_info()
-
-
+Dara.deposit(3000).deposit(1000).deposit(5000).withdraw(4000).yield_interest(0.8).display_account_info()
+Amira.deposit(4000).deposit(6000).withdraw(100).withdraw(50).withdraw(50).withdraw(30).display_account_info()
 
 
