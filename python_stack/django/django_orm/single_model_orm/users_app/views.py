ee75697ef75b3e416f386a5d698a8a3ed0173c61
Dara <dararateb93@gmail.com>
2021-05-20 16:50:35 +0300

    Python Assignments

--- python_stack/django/django_orm/single_model_orm/users_app/views.py
+++ python_stack/django/django_orm/single_model_orm/users_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Users
 
-# Create your views here.
+def form(request):
+    context = {
+        "Guys": Users.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def add(request):
+    Users.objects.create(firstname=request.POST["firstname"],
+    last_name=request.POST["last_name"],email_address=request.POST["email_address"],age=int(request.POST["age"]))
+    return redirect("/")
