3ee6f06583f6acc1ffef6f0fe6a644de9606c70e
Dara <dararateb93@gmail.com>
2021-05-22 23:34:09 +0300

    Python Assignments

--- python_stack/django/django_intro/ninja_gold/ninja_app/urls.py
+++ python_stack/django/django_intro/ninja_gold/ninja_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('',views.index),
     path('process_money',views.process)
 
-}
+]
--- python_stack/django/django_intro/ninja_gold/ninja_app/views.py
+++ python_stack/django/django_intro/ninja_gold/ninja_app/views.py
@@ -4,8 +4,9 @@ from datetime import datetime
 
 # Create your views here.
 def index(request):
-    if 'gold' not in request.session:
+    if 'gold' not in request.session or 'activites' not in request.session:
         request.session['gold']=0
+        request.session['activities']=[]
     return render (request,'index.html')
 
 def process(request):
@@ -13,12 +14,19 @@ def process(request):
     if request.method=='POST':
         if request.POST['place']=='farm':
             x=random.randint(10,20)
+            request.session['activities'].append('You have earned ' + str(x) + ' golds from the farm' + ' ' + '(' + str(datetime.now().strftime("%Y-%m-%d %H:%M")) + ')')
         if request.POST['place']=='cave':
             x=random.randint(5,10)
+            request.session['activities'].append('You have earned ' + str(x) + ' golds from the farm' + ' ' + '(' + str(datetime.now().strftime("%Y-%m-%d %H:%M")) + ')')
         if request.POST['place']=='house':
             x=random.randint(2,5)
+            request.session['activities'].append('You have earned ' + str(x) + ' golds from the farm' + ' ' + '(' + str(datetime.now().strftime("%Y-%m-%d %H:%M")) + ')')
         if request.POST['place']=='casino':
             x=random.randint(-50,50)
+            if x >0:
+                request.session['activities'].append('You have earned ' + str(x) + ' golds from the farm' + ' ' + '(' + str(datetime.now().strftime("%Y-%m-%d %H:%M")) + ')')
+            if x<0:
+                request.session['activities'].append('You have lost ' + str(x) + ' golds from the farm' + ' ' + '(' + str(datetime.now().strftime("%Y-%m-%d %H:%M")) + ')')
 
     request.session['gold'] += x
-    return redirect('')
+    return redirect('/')
