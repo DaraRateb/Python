30633955db6b37107655ca5aa616ceacd99a2c73
Dara <dararateb93@gmail.com>
2021-05-23 16:26:31 +0300

    Python Assignments

---
+++ python_stack/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+class Dojo(models.Model):
+    name=models.CharField(max_length=45)
+    city=models.CharField(max_length=45)
+    state=models.CharField(max_length=45)
+    desc=models.CharField(max_length=45)
+# Create your models here.
+class Ninjas(models.Model):
+    first_name=models.CharField(max_length=45)
+    last_name=models.CharField(max_length=45)
+    dojokey = models.ForeignKey(Dojo, related_name="ninja", on_delete = models.CASCADE)
+
+# Create your models here.
